490e08c37452fafbfec138824cfad23407015338
mark chaitin <mchaitin@gmail.com>
2020-06-14 19:24:00 -0700

    Updates

---
+++ precache-manifest.c918f1f627aa103ad4ba8badf612de6e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bf0f020574151ad076510b42c58dec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54a854af95adcd4a2bea",
+    "url": "/static/css/main.dc6ae21c.chunk.css"
+  },
+  {
+    "revision": "727d2a45a3268c4c904d",
+    "url": "/static/js/2.09aac26e.chunk.js"
+  },
+  {
+    "revision": "54a854af95adcd4a2bea",
+    "url": "/static/js/main.311de3a7.chunk.js"
+  },
+  {
+    "revision": "90b482eecc1255364260",
+    "url": "/static/js/runtime-main.ac619f94.js"
+  },
+  {
+    "revision": "ff431c0a94ca0e3104c9279b7facee8e",
+    "url": "/static/media/Annie.ff431c0a.jpg"
+  },
+  {
+    "revision": "f93f2f2fba558aa03c819c9c46e54244",
+    "url": "/static/media/Molly.f93f2f2f.jpg"
+  },
+  {
+    "revision": "6e22b353d22bd4c4b16e593e454672c4",
+    "url": "/static/media/Performance.6e22b353.jpg"
+  },
+  {
+    "revision": "5f3bf7306c45df074430c364afd9a092",
+    "url": "/static/media/Zosia.5f3bf730.jpg"
+  }
+]);
--- src/utils/StringHelper.js
+++
@@ -1,8 +0,0 @@
-import locale from '../resources/locales/en-US.json';
-
-const translateString = (key) => {
-  if (key in locale) return locale[key];
-  return key;
-};
-
-export default translateString;
